00d38816cb1a6cb91c4400de0e8b329007574495
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2020-03-11 15:02:13 -0500

    implemented search capabilities

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -23,6 +23,24 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @RequestMapping(value="results")
+    public String displaySearchResults (Model model, String searchType, String searchTerm){
+
+        ArrayList<Job> jobs;
+
+        if(searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()){
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+
+
+        return "results";
+    }
+
 
 }
